5fd9af5632bb7960787b23131a1bbc9741e09d20
Promart1 <marta.batih@gmail.com>
2023-08-16 15:27:34 +0300

    add hw 3

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,26 +6,37 @@ const {
   addContact,
   removeContact,
   updateContact,
+  updateStatusContact,
 } = require("../../controllers/contacts");
 const validateBody = require("../../middlewares/validateBody");
 const contactSchema = require("../../schemas/contact");
 const checkBody = require("../../middlewares/checkBody");
+const { addSchema, updateFavoriteSchema } = require("../../models/contact");
+const isValidId = require("../../middlewares/isValidId");
 
 const router = express.Router();
 
 router.get("/", listContacts);
 
-router.get("/:contactId", getContactById);
+router.get("/:contactId", isValidId, getContactById);
 
-router.post("/", checkBody, validateBody(contactSchema), addContact);
+router.post("/", checkBody, validateBody(addSchema), addContact);
 
-router.delete("/:contactId", removeContact);
+router.delete("/:contactId", isValidId, removeContact);
 
 router.put(
   "/:contactId",
   checkBody,
-  validateBody(contactSchema),
+  validateBody(addSchema),
+  isValidId,
   updateContact
+);
+
+router.patch(
+  "/:id/favorite",
+  validateBody(updateFavoriteSchema),
+  isValidId,
+  updateStatusContact
 );
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const app = require("./app");
 
 const mongoose = require("mongoose");
 
-const { DB_Host } = process.env;
+const { DB_Host, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_Host)
   .then(() => {
     console.log("Database connection successful");
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Server running. Use our API on port: 3000");
     });
   })
